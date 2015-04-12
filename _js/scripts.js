8cac62ceba4dd452661bacb590f32b2921a491ad
StephanieArgy <steph@mentalslapstick.com>
2015-04-11 22:56:21 -0700

    Starter files and images for project

---
+++ _js/scripts.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  var greeting="Hello!";
+  document.write(greeting);
+});
+
+
+<script type="text/javascript">
+	jQuery(function($){
+    	     $( '.menu-btn' ).click(function(){
+    	     $('.responsive-menu').toggleClass('expand')
+    	     })
+        })
+</script>
